9ceca156aaef01d0d41444a7ab1b5980880f0dec
Relsanz <fwidiyatnaadmaja@gmail.com>
2025-02-26 06:41:38 +0700

    Update MainActivity.java

--- flutter_AppCoffe/android/app/src/main/java/com/flutter/reference/MainActivity.java
+++ flutter_AppCoffe/android/app/src/main/java/com/flutter/reference/MainActivity.java
@@ -1,13 +1,7 @@
 package com.flutter.reference;
 
-import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-    GeneratedPluginRegistrant.registerWith(flutterEngine);
-  }
+  // Tidak perlu override configureFlutterEngine untuk Android embedding v2.
 }
